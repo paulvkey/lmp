36b9ab51772ef0471515fb6041bcae5c43505b7a
paulvkey <vkeyroux@gmail.com>
2025-11-26 10:39:06 +0800

    update sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+cd vue
+
+npm install
+
+sh requirments.sh
--- vue/requirments.sh
+++ vue/requirments.sh
@@ -2,19 +2,17 @@
 
 npm install element-plus -S
 npm install @element-plus/icons-vue -D
-npm install sass -D
 npm install unplugin-auto-import -D
 npm install unplugin-element-plus -D
 npm install unplugin-vue-components -D
+npm install @vitejs/plugin-vue-jsx -D
+npm install sass -D
+
 npm install dayjs --S
 npm install axios -S
-npm install pinia -S
-npm install pinia-plugin-persistedstate -S
-npm install @vitejs/plugin-vue-jsx -D
-npm install @wangeditor/editor -S
-npm install @wangeditor/editor-for-vue@next -S
-npm install marked highlight.js dompurify -S
-npm install markdown-it markdown-it-highlightjs markdown-it-katex github-markdown-css -S
-npm install vue-renderer-markdown @iconify/vue @vueuse/core katex mermaid vue-use-monaco stream-markdown shiki vite-plugin-monaco-editor-esm -S
+npm install pinia pinia-plugin-persistedstate -S
 
-npm install normalize.css -S
+npm install marked highlight.js dompurify -S
+npm install markdown-it markdown-it-highlightjs markdown-it-katex github-markdown-css katex -S
+
+# npm install vue-renderer-markdown @iconify/vue @vueuse/core mermaid vue-use-monaco stream-markdown shiki vite-plugin-monaco-editor-esm -S
